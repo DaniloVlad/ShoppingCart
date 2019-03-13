95850ee5fe2af573ca8e98470885a2697a7c09f4
danilovlad <danilo.vladicic@gmail.com>
2019-03-13 14:59:10 -0400

    Alter table

--- server/routes/dashboard/order.js
+++ server/routes/dashboard/order.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const {captureOrder} = require('../../middlewares/payment');
-
+const Order = require('../../models/orders');
 var router = express.Router();
 
 router.post('/order', (req, res, next) => {
@@ -14,11 +14,19 @@ router.post('/order', (req, res, next) => {
         }
         else {
             var token = result.result.id; //paypal order id
+            var status = result.result.status; //Paypal payment status
             var order_id = result.result.purchase_units[0].reference_id; //internal order id
             var transaction_id = result.result.purchase_units[0].payments.captures[0].id; //admin transaction id
-            console.log('Order_id: '+token+" Reference_ID: "+order_id+ " Transaction id: "+transaction_id)
-            console.log(result.status)
-            if(result.result.statusCode === 201) return res.json({suc: "Successfully captured order!"})
+            console.log('Order_id: ' + token 
+            + " Reference_ID: " + order_id 
+            + " Transaction id: "+transaction_id 
+            + " Status: " + status);
+            if(status === 'COMPLETED') {
+                console.log('Updating order status');
+                Order.updateTransactionId(order_id, transaction_id)
+                .then(results2 => res.json({succ: "Successfully captured order!"}))
+                .catch(err => next('Error: Couldnt capture order!'))
+            }
         }
     })
     res.json({suc: 'test webhook!'})
