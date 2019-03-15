34dc276dca1fb36886aa85947ae57fb7fb03e8b7
danilovlad <danilo.vladicic@gmail.com>
2019-03-15 11:51:17 -0400

    Comment & Refactor

--- server/app.js
+++ server/app.js
@@ -1,49 +1,40 @@
-var express = require('express');
-var session = require('express-session');
-var connection = require('./models/connection');
-var MySQLStore = require('express-mysql-session')(session);
-var bodyParser = require('body-parser');
-var morgan = require('morgan');
-
-
-
-var api = require('./routes/api');
-var dasboard = require('./routes/dashboard');
-
-var app = express();
-
-var sessionStore = new MySQLStore({}, connection);
-
-var sessionOpts = {
-    secret: "daniloseag", //make random string
+const express = require('express');
+const session = require('express-session');
+const bodyParser = require('body-parser');
+const morgan = require('morgan');
+const MySQLStore = require('express-mysql-session')(session);
+const connection = require('./models/connection');
+const api = require('./routes/api');
+//Create app
+const app = express();
+//Configure session options
+const sessionStore = new MySQLStore({}, connection);
+const sessionOpts = {
+    secret: "superhiddensecret", //make random string
     saveUninitialized: true,
     resave: false,
     cookie: {secure: false},
     store: sessionStore
 }
-
-
+//Configure App settings
 app.set('trust proxy', 1);
 app.use(morgan("dev"));
 app.use(bodyParser.json());
 app.use(session(sessionOpts));
-
-
+//Ensure cart is defined for new requests
 app.use((req, res, next) => {
     if(req.session.cart === undefined)
         req.session.cart = [];
     next();
 });
-
+//Mount main routes
 app.use('/api', api);
-
-app.use('/dashboard', dasboard);
-
+//Router error handler
 app.use((err, req, res, next) => {
     console.error(err);
     return res.json({err: err});
 });
-
+//Start app on port 3001
 app.listen(3001, () => {
     console.log("Starting server on port 3001...");
 });
